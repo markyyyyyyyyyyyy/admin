9d36dd39e136cb377d5704f1d2ab4d416173492a
markyyyyyyyyyyyy <161038977+markyyyyyyyyyyyy@users.noreply.github.com>
2024-03-23 00:17:06 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,114 @@
+<?php 
+include 'config.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+
+   
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+  
+   $select = " SELECT * FROM admin_user WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_array($result);
+
+      if($row['user_type'] == 'admin'){
+
+        
+         header('location:index.php');
+
+      }elseif($row['user_type'] == 'doctor'){
+
+         
+         header('location:doctors-dashboard.php');
+
+      }
+   }else{
+      $error[] = 'incorrect email or password!';
+     
+   }
+
+
+};
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Login - SB Admin</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Admin Login</h3></div>
+                                    <div class="card-body">
+                                        <form action="" method = "post">
+                                        <?php
+                                        if(isset($error)){
+                                            foreach($error as $error){
+                                                echo '<span class="error-msg">'.$error.'</span>';
+                                            };
+                                        };
+                                            ?>
+    
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" name="email">
+                                                <label for="inputEmail">Email address</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" name="password">
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                            <div class="form-check mb-3">
+                                                <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
+                                                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="password.php">Forgot Password?</a>
+                                               <input type="submit" class="submit" name="submit" value="Login"> 
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="register.php">Need an account? Sign up!</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Toothbook Dental Clinic</div>
+                            <div>
+                              
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
